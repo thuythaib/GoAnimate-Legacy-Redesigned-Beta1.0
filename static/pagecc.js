2dbf7a9156ff83fc1e122d2f137760b4059ee888
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-05-17 08:36:18 -0400

    Update pagecc.js

--- static/pagecc.js
+++ static/pagecc.js
@@ -48,15 +48,15 @@ module.exports = function (req, res, url) {
 					storePath: process.env.STORE_URL + "/<store>",
 					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
 					original_asset_id: query["id"] || null,
-					themeId: "business",
-					ut: 60,
-					bs: "default",
+					themeId: "family",
+					ut: 23,
+					bs: "adam",
 					appCode: "go",
 					page: "",
 					siteId: "go",
 					m_mode: "school",
 					isLogin: "Y",
-					isEmbed: 1,
+					isEmbed: 0,
 					ctc: "go",
 					tlang: "en_US",
 				},
@@ -90,14 +90,14 @@ module.exports = function (req, res, url) {
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-            <a class="navbar-brand" href="/dashboard/videos" title="GoAnimate Legacy Redesigned">
+            <a class="navbar-brand" href="/dashboard/videos.html" title="GoAnimate Legacy Redesigned">
                 <img src="/html/img/logo.png" alt="GoAnimate Legacy Redesigned">
             </a>
         </div>
 
         <ul class="nav site-nav-alert-nav hidden-xs">
             <li>
-                <a href="/notifications" title="Notifications"><span class="glyph-pro glyph-bell"></a>
+                <a href="/notifications.html" title="Notifications"><span class="glyph-pro glyph-bell"></a>
             </li>
         </ul>
         <div class="collapse navbar-collapse navbar-ex1-collapse">
@@ -105,26 +105,26 @@ module.exports = function (req, res, url) {
                 <li class="dropdown">
                     <a class="dropdown-toggle" href="#" data-toggle="dropdown">Your Account <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="/student">Dashboard</a></li>
-                        <li><a href="/dashboard/videos">Your Videos</a></li>
+                        <li><a href="/student.html">Dashboard</a></li>
+                        <li><a href="/dashboard/videos.html">Your Videos</a></li>
                         <li class="divider"></li>
-                        <li><a href="/account">Account Settings</a></li>
-                        <li><a href="/profile/you">Your Profile</a></li>
+                        <li><a href="/account.html">Account Settings</a></li>
+                        <li><a href="/profile/you.html">Your Profile</a></li>
                         <li class="divider"></li>
-                        <li><a class="logout-link" href="/logoff">Logout</a></li>
+                        <li><a class="logout-link" href="/logoff.html">Logout</a></li>
                     </ul>
                 </li><li class="dropdown">
                     <a class="dropdown-toggle" href="#" data-toggle="dropdown">Explore <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="/students">Students</a></li>
-                        <li><a href="/teachers">Teachers</a></li>
-                        <li><a href="/videos">Videos</a></li>
-                        <li><a href="/public_faq">FAQ</a></li>
+                        <li><a href="/students.html">Students</a></li>
+                        <li><a href="/teachers.html">Teachers</a></li>
+                        <li><a href="/videos.html">Videos</a></li>
+                        <li><a href="/public_faq.html">FAQ</a></li>
                     </ul>
                 </li>
                 <li>
-				<a class="hidden-sm hidden-md hidden-lg" href="/v/videomaker">Make a Video</a>
-				<span class="site-nav-btn hidden-xs"><a class="btn btn-orange" href="/v/videomaker">Make a Video</a></span>
+				<a class="hidden-sm hidden-md hidden-lg" href="/html/videomaker.html">Make a Video</a>
+				<span class="site-nav-btn hidden-xs"><a class="btn btn-orange" href="/html/videomaker.html">Make a Video</a></span>
                 </li>
             </ul>
         </div>
@@ -134,14 +134,13 @@ module.exports = function (req, res, url) {
 
 
         <ul class="breadcrumb">
-            <li><a href="/c/create">Make a video</a></li>
+            <li><a href="/html/create.html">Make a video</a></li>
             <li><a href="/character_creator/">Your Characters</a></li>
             <li class="active">Create a new character</li>
         </ul>
 
 <div>
-    <div id="char_creator_client" align="center">
-${toObjectString(attrs, params)}
+    <div id="char_creator_client" align="center">${toObjectString(attrs, params)}
 </div>
 </div>
 </div>		
@@ -152,11 +151,11 @@ ${toObjectString(attrs, params)}
                 <div class="site-footer-nav-col">
                     <h4><span>About GoAnimate</span></h4>
                     <ul class="list-unstyled">
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/about">Who we are</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://resources.goanimate.com/careers/">Careers</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/contactus">Contact Us</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://blog.goanimate.com/">Blog</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://press.goanimate.com/">Press</a></li>
+                        <li><a href="https://josephcrosmanplays532.github.io/about">Who we are</a></li>
+                        <li><a href="https://resources.josephcrosmanplays532.github.io/careers/">Careers</a></li>
+                        <li><a href="https://josephcrosmanplays532.github.io/contactus">Contact Us</a></li>
+                        <li><a href="https://blog.josephcrosmanplays532.github.io/">Blog</a></li>
+                        <li><a href="https://press.josephcrosmanplays532.github.io/">Press</a></li>
                     </ul>
                 </div>
             </div>
@@ -164,9 +163,9 @@ ${toObjectString(attrs, params)}
                 <div class="site-footer-nav-col">
                     <h4><span>GoAnimate Solutions</span></h4>
                     <ul class="list-unstyled">
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/business/videoplans?hook=footer_button.site">Plans and Pricing</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/business/videoplans/federal?hook=footer_button.site">Plans for Federal Agencies</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate4schools.com/" target="_blank">GoAnimate for Schools</a></li>
+                        <li><a href="https://discord.gg/goanimate?hook=footer_button.site">Plans and Pricing</a></li>
+                        <li><a href="https://discord.io/goanimate4schools?hook=footer_button.site">Plans for Federal Agencies</a></li>
+                        <li><a href="https://goanimateforschools.github.io/" target="_blank">GoAnimate for Schools</a></li>
                     </ul>
                 </div>
             </div>
@@ -174,9 +173,9 @@ ${toObjectString(attrs, params)}
                 <div class="site-footer-nav-col">
                     <h4><span>Usage Guidelines</span></h4>
                     <ul class="list-unstyled">
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/termsofuse">Terms of Service</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://goanimate.com/privacy">Privacy Policy</a></li>
-                        <li><a href="//web.archive.org/web/20180307222818/https://support.goanimate.com/hc/en-us/articles/202408574" target="_blank">Cancellation Policy</a></li>
+                        <li><a href="https://josephcrosmanplays532.github.io/termsofuse">Terms of Service</a></li>
+                        <li><a href="https://josephcrosmanplays532.github.io/privacy">Privacy Policy</a></li>
+                        <li><a href="//support.josephcrosmanplays532.github.io/hc/en-us/articles/202408574" target="_blank">Cancellation Policy</a></li>
                     </ul>
                 </div>
             </div>
@@ -184,8 +183,8 @@ ${toObjectString(attrs, params)}
                 <div class="site-footer-nav-col">
                     <h4>Getting Help</h4>
                     <ul class="list-unstyled">
-                        <li><a href="http://web.archive.org/web/20180307222818/https://resources.goanimate.com/">Resources</a></li>
-                        <li><a href="http://web.archive.org/web/20180307222818/https://support.goanimate.com/">Help Center</a></li>
+                        <li><a href="https://resources.josephcrosmanplays532.github.io/">Resources</a></li>
+                        <li><a href="https://support.josephcrosmanplays532.github.io/">Help Center</a></li>
                     </ul>
                 </div>
             </div>
@@ -196,17 +195,17 @@ ${toObjectString(attrs, params)}
                 <div class="site-footer-socials-container">
                     Follow us on:
                     <ul class="site-footer-socials">
-                        <li><a class="glyph-social glyph-facebook" href="http://web.archive.org/web/20180307222818/https://www.facebook.com/GoAnimateInc"><span class="sr-only">Facebook</span></a></li>
-                        <li><a class="glyph-social glyph-twitter" href="http://web.archive.org/web/20180307222818/https://twitter.com/GoAnimate"><span class="sr-only">Twitter</span></a></li>
-                        <li><a class="glyph-social glyph-linked-in" href="http://web.archive.org/web/20180307222818/https://www.linkedin.com/company/goanimate"><span class="sr-only">LinkedIn</span></a></li>
-                        <li><a class="glyph-social glyph-google-plus" href="http://web.archive.org/web/20180307222818/https://plus.google.com/+goanimate"><span class="sr-only">Google+</span></a></li>
-                        <li><a class="glyph-social glyph-youtube" href="http://web.archive.org/web/20180307222818/https://www.youtube.com/user/GoAnimate"><span class="sr-only">YouTube</span></a></li>
+                        <li><a class="glyph-social glyph-facebook" href="https://www.facebook.com/GoAnimateInc"><span class="sr-only">Facebook</span></a></li>
+                        <li><a class="glyph-social glyph-twitter" href="https://twitter.com/GoAnimate"><span class="sr-only">Twitter</span></a></li>
+                        <li><a class="glyph-social glyph-linked-in" href="https://www.linkedin.com/company/goanimate"><span class="sr-only">LinkedIn</span></a></li>
+                        <li><a class="glyph-social glyph-google-plus" href="https://plus.google.com/+goanimate"><span class="sr-only">Google+</span></a></li>
+                        <li><a class="glyph-social glyph-youtube" href="https://www.youtube.com/user/GoAnimate"><span class="sr-only">YouTube</span></a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-sm-6">
                 <div class="site-footer-copyright">
-                    <img src="http://web.archive.org/web/20180307222818im_/https://d3v4eglovri8yt.cloudfront.net/static/f789d2b2551c1fb6/go/img/footer/logo_amazon.png" alt="AWS Partner Network">
+                    <img src="https://d3v4eglovri8yt.cloudfront.net/static/add8e214e09bd155/go/img/footer/logo_amazon.png" alt="AWS Partner Network">
                     &nbsp;&nbsp;&nbsp;
                     <div class="ga-copy">GoAnimate © 2021</div>
                 </div>
@@ -214,7 +213,7 @@ ${toObjectString(attrs, params)}
         </div>
     </div>
 </div>
-</body>${stuff.pages[url.pathname] || ""}`
+</div>${stuff.pages[url.pathname] || ""}`
 	);
 	return true;
 };
